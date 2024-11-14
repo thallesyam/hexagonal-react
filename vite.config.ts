cfce8f958c5dd8b77d747b3e6d2a23048630fa09
Thalles Ian <thallesyam@gmail.com>
2024-11-13 23:24:08 -0300

    chore: config relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./_vitest_/setup.js"]
+  },
+  resolve: {
+    alias: {
+      find: "@",
+      replacement: path.resolve(__dirname, "src"),
+    },
   }
 })
